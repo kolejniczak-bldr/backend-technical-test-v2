d3fdd4da13b1a451912216956a63aaa598e36c64
kolejniczak <korneliusz.olejniczak@boldare.com>
2021-11-08 12:22:10 +0100

    refactor(test): Separated token constants to their own class

---
+++ backend-technical-test-rest-assured/src/test/java/com/tui/jwt/TokenConstants.java
@@ -0,0 +1,9 @@
+package com.tui.jwt;
+
+public class TokenConstants {
+  public static final String VALID_TOKEN =
+      "eyJhbGciOiJIUzUxMiJ9.eyJpc1ZhbGlkIjp0cnVlfQ.3sN4GFDnU7tYgLA_SxazOQQca1X3fTW4ColSxkP2XONZrzHMwc2AaRvxjPcV-Up51-eUsSV94BjEfh5wjnNtBQ";
+  public static final String INVALID_TOKEN =
+      "eyJhbGciOiJIUzUxMiJ9.eyJpc1ZhbGlkIjpmYWxzZX0.l0ZS55nKy5UKDF4bp6K4_QwfUquZeBmMPYmG9prEHwbdnO5H91HLne9Hx3bvKt_wxgJUxwIwSK8VxwQWXqzFvg";
+  public static final String INVALID_FORMATTING_TOKEN = "broken";
+}
--- backend-technical-test-rest-assured/src/test/java/com/tui/order/OrdersSecurityTests.java
+++ backend-technical-test-rest-assured/src/test/java/com/tui/order/OrdersSecurityTests.java
@@ -6,17 +6,12 @@ import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
 
+import com.tui.jwt.TokenConstants;
 import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OrdersSecurityTests {
-
-  public static final String VALID_TOKEN =
-      "eyJhbGciOiJIUzUxMiJ9.eyJpc1ZhbGlkIjp0cnVlfQ.3sN4GFDnU7tYgLA_SxazOQQca1X3fTW4ColSxkP2XONZrzHMwc2AaRvxjPcV-Up51-eUsSV94BjEfh5wjnNtBQ";
-  public static final String INVALID_TOKEN =
-      "eyJhbGciOiJIUzUxMiJ9.eyJpc1ZhbGlkIjpmYWxzZX0.l0ZS55nKy5UKDF4bp6K4_QwfUquZeBmMPYmG9prEHwbdnO5H91HLne9Hx3bvKt_wxgJUxwIwSK8VxwQWXqzFvg";
-  public static final String INVALID_FORMATTING_TOKEN = "broken";
 
   @DisplayName("Orders endpoint respond with bad request, with no credentials")
   @Test
@@ -28,7 +23,7 @@ class OrdersSecurityTests {
   @Test
   void getAllWithValidToken() {
     given()
-        .header(HttpHeaders.AUTHORIZATION, VALID_TOKEN)
+        .header(HttpHeaders.AUTHORIZATION, TokenConstants.VALID_TOKEN)
         .when()
         .get("/order")
         .then()
@@ -39,7 +34,7 @@ class OrdersSecurityTests {
   @Test
   void getAllWithInValidToken() {
     given()
-        .header(HttpHeaders.AUTHORIZATION, INVALID_TOKEN)
+        .header(HttpHeaders.AUTHORIZATION, TokenConstants.INVALID_TOKEN)
         .when()
         .get("/order")
         .then()
@@ -50,7 +45,7 @@ class OrdersSecurityTests {
   @Test
   void getAllWithInValidFormattingToken() {
     given()
-        .header(HttpHeaders.AUTHORIZATION, INVALID_FORMATTING_TOKEN)
+        .header(HttpHeaders.AUTHORIZATION, TokenConstants.INVALID_FORMATTING_TOKEN)
         .when()
         .get("/order")
         .then()
